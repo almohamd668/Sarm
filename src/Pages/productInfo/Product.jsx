14a8e3dba3f2dab8e883233bcade8bbfa7099b53
Mohmmed Mhmpud <almohamd668@gmail.com>
2024-09-11 10:59:02 -0700

    product responsive

--- src/Pages/productInfo/Product.jsx
+++ src/Pages/productInfo/Product.jsx
@@ -2,88 +2,100 @@ import { Link } from "react-router-dom";
 import vid1 from "../../assets/12.mp4";
 import img1 from "../../assets/p-1.jpg";
 import "./product.css";
-
+// <strong className="ps-4">{product.num_of_pieces}</strong>
+// {product.pname}
+// {product.description}
+// {`Price :  ${product.price} $`}
 const Product = ({ product, showButton }) => {
   return (
     <section className="">
-      <div className="dad-card grid grid-cols-3 w-full mx-auto my-10 rounded-lg">
-      <div className="con-img bg-gradient-to-r from-darkSec h-full to-primary  w-full rounded-lg overflow-hidden" >
+      <div className="container grid grid-cols-1 md:grid-cols-2 w-full mx-auto my-10 max-w-full md:min-w-[500px] min-h-[400px] rounded-lg ">
+        <div
+          className="con-img
+      bg-gradient-to-r
+    from-darkSec h-full md:min-h-[300px]
+    to-primary  w-full max-w-full
+      rounded-lg overflow-hidden"
+        >
           <img
-            // src={product.image}
             src={img1}
-            className="card-img transform hover:rotate-6 mx-auto"
-            alt={product.title}
+            // src={product.img_url}
+            className="card-img transform hover:rotate-6 mx-auto max-w-full"
+            // alt={product.title}
           />
         </div>
-      
-        <div className="con-card bg-darkSec
-         text-black text-sm border-0 w-full h-5/6 my-auto rounded-tr-lg rounded-br-lg p-5">
-          <div className="address my-10">
-          <h1 className="text-5xl font-bold text-white my-5">Hellooooo Frooooooo Eyadooooo</h1>
-          <p className="text-lg text-white">1-customer review | add review</p>
+        <div
+          className="con-card bg-darkSec
+     text-black text-sm border-0 w-full 
+     my-auto rounded-tr-lg rounded-br-lg p-5 h-fit min-h-[300px] max-w-full"
+        >
+          <div className="address my-5 flex justify-between items-center">
+            <h1 className="text-xl md:text-3xl  font-bold  text-white my-5">
+              gcvxcc
+            </h1>
+            <h3 className="text-sm md:text-lg text-white">Contain </h3>
           </div>
-          <h2 className="my-4 text-3xl text-white">Hellooooo Frooooooo Eyadooooo</h2>
-          {showButton ? (
-            <Link
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              to={`product-info/${product.id}`}
-            >
-              info
-            </Link>
-          ) : (
-            <p className="text-lg leading-6 text-white mt-3 mb-10">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates accusantium asperiores cumque recusandae quaerat, porro doloribus beatae explicabo in natus alias itaque expedita et consequuntur unde eum quasi quod. Optio?</p>
-          )}
-          <div className="add-cart">
-            <div className="button-cart inline-block">
-              <button className="py-2 px-4  text-white border-solid border-[1px] border-black">-</button>
-              <button className="py-2 px-4  text-white border-solid border-[1px] border-black">5</button>
-              <button className="py-2 px-4  text-white border-solid border-[1px] border-black">+</button>
-            </div>
-          <button className="bg-black text-white mx-5 py-2 px-4">
-          ADD TO CART
-          </button>
-          </div>
-          
+          <h4 className="my-1 text-2xl md:text-4xl font-bold  text-white/95">
+            xcvxcvxcSD
+          </h4>
+          <p className="text-sm md:text-[18px] leading-[1.3rem] text-white mt-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+            saepe iure rerum amet laudantium? Illo, error sit placeat nesciunt
+            ab accusantium similique. Libero possimus nobis explicabo nam, eum
+            exercitationem sequi!
+          </p>
         </div>
-        </div>
-          <div className="con-vid grid grid-cols-2 px-10 items-end">
-      <div className="con-bar h-[200px] grid grid-rows-3 items-end w-full gap-5 ">
-        <div className="con-bro1 grid w-full grid-rows-2 gap-3">
-          <h3 className="text-white indent-5">html</h3>
-          <div className="shadow-sm shadow-secondary bg-slate-600 h-6 rounded-full flex">
+      </div>
+
+      <div className="con-vid grid grid-cols-1 md:grid-cols-2  px-10 items-end">
+        <div className="con-bar h-[200px] grid grid-rows-3 items-end w-full gap-5 ">
+          <div className="con-bro1 grid w-full grid-rows-2 gap-3">
+            <h3 className="text-white indent-5">html</h3>
+            <div className="shadow-sm shadow-secondary bg-slate-600 h-6 rounded-full flex">
               <div className="bro-1 bg-gradient-to-r from-primary to-secondary w-[1] h-full flex justify-center items-center text-white font-bold rounded-full text-xl">
-              50%
+                50%
               </div>
+            </div>
           </div>
-        </div>
-        <div className="con-bro2 grid w-full grid-rows-2 gap-3">
-        <h3 className="text-white indent-5">css</h3>
-          <div className="shadow-sm shadow-secondary bg-slate-600 h-6 rounded-full flex">
+          <div className="con-bro2 grid w-full grid-rows-2 gap-3">
+            <h3 className="text-white indent-5">css</h3>
+            <div className="shadow-sm shadow-secondary bg-slate-600 h-6 rounded-full flex">
               <div className="bro-2 bg-gradient-to-r from-primary to-secondary w-[1] h-full flex justify-center items-center text-white font-bold rounded-full text-xl">
-              90%
+                90%
               </div>
+            </div>
           </div>
-        </div>
-        <div className="con-bro3 grid w-full grid-rows-2 gap-3">
-          <h3 className="text-white indent-5">javascript</h3>
-          <div className="shadow-sm shadow-secondary bg-slate-600 h-6 rounded-full flex">
+          <div className="con-bro3 grid w-full grid-rows-2 gap-3">
+            <h3 className="text-white indent-5">javascript</h3>
+            <div className="shadow-sm shadow-secondary bg-slate-600 h-6 rounded-full flex">
               <div className="bro-3 bg-gradient-to-r from-primary to-secondary w-[1] h-full flex justify-center items-center text-white font-bold rounded-full text-xl">
-              70%
+                70%
               </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className=" w-full h-[200px] mt-[100px] me-auto flex justify-end mx-10">
-        <video
-          src="https://res.cloudinary.com/dqrzcmaxm/video/upload/v1724429046/12_chd9rw.mp4"
-          controls
-          autoPlay
-          loop
-          muted
-          className=" h-[200px]"
-          width={700}
-        ></video>
-      </div>
+
+        <div className=" w-full h-[200px] mt-[100px] mx-auto md:me-auto flex justify-center md:justify-end  mx-10">
+          <div className="mySwiper flex items-center justify-center relative mx-2">
+            <video
+              src={vid1}
+              loop
+              muted
+              autoPlay
+              className=" h-[300px] md:h-[400px] w-full md:w-[90%] object-cover rounded-[20%] bg-opacity-10"
+            ></video>
+            <div className="absolute inset-0 flex items-end justify-center pb-4">
+              <span
+                className="block text-lg md:text-4xl font-bold italic bg-gradient-to-r
+           from-primary via-cyan/80 to-[#5e33ff] bg-200% text-transparent animate-gradientX z-50"
+                style={{
+                  backgroundClip: "text",
+                  WebkitBackgroundClip: "text",
+                }}
+              ></span>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
