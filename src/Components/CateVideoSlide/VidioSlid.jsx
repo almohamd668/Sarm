d112a023517277d6e0836388208487a04c1ec04c
Mohmmed Mhmpud <almohamd668@gmail.com>
2024-09-12 10:25:19 -0700

    moh

--- src/Components/CateVideoSlide/VidioSlid.jsx
+++ src/Components/CateVideoSlide/VidioSlid.jsx
@@ -14,9 +14,11 @@ import { Autoplay } from "swiper/modules";
 
 const TestimonialsSlid = () => {
   const [products, setProducts] = useState([]);
+  let vidApi = "get_videos.php";
+
   const [categories, setCategories] = useState([]);
   //axios.defaults.baseURL = "https://titansmaxplus.com/titans/api/"
-  let vidApi = "get_videos.php";
+
   let catApi = "categories.php"; //, name = null
   const fetchFun = async (url, setState, name = null) => {
     console.log(name);
@@ -41,8 +43,6 @@ const TestimonialsSlid = () => {
      
     
       <div className="py-[60px]">
-      
-    
         <div
           className="absolute inset-0 z-[-1]  bg-cover  bg-center bg-no-repeat h-full w-full"
           style={{
@@ -51,7 +51,6 @@ const TestimonialsSlid = () => {
        rgba(0, 0, 0, 0.393)),
         url("https://cdn.msaaq.com/pages/242547/asset-pink-bg.svg")`,
             backgroundAttachment: "fixed",
-          
           }}
         ></div>
         <div className=" element-center  container md:ps-26 flex-col md:flex-row gap-3 md:gap-6 justify-around">
@@ -59,11 +58,10 @@ const TestimonialsSlid = () => {
               <h2 className=" text-2xl md:text-3xl md:ms-9 w-[270px] text-center  md:font-bold text-transparent text-trans1">
                  OUR PRODUCTS
               </h2>
-                <div className="element-center md:flex-col flex-wrap items-center md:items-start md:ps-10 gap-2
-                  md:gap-4 my-5  md:gap-x-3 max-w-full w-[280px] md:w-auto">
+                <div className="element-center md:flex-col flex-wrap 
+                items-center md:items-start md:ps-10 gap-2
+                md:gap-4 my-5  md:gap-x-3 max-w-full w-[280px] md:w-auto">
                   
-                
-           
             <button
               onClick={() => {
                 fetchFun(vidApi, setProducts);
--- src/Pages/Products/ProductCart.jsx
+++ src/Pages/Products/ProductCart.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
 // import img1 from "../../assets/p-2-rebg.png";
 
-
 // import "./Product.css";
 const truncateText = (text, wordLimit) => {
   const words = text.split(" ");
   if (words.length > 3) {
-    return words.slice(1, 2).join(" ") + "           ........." ;
+    return words.slice(1, 2).join(" ") + "           .........";
   }
   return text;
 };
 
 const ProductCart = ({ product, showButton }) => {
+  console.log(product);
   return (
     <section className="sec-card neon-effect">
       <div className="bg-darkSec rounded-lg shadow-md text-white text-sm border-0 element-center grid grid-cols-1 hover:border-blue-500 m-0 p-0">
@@ -23,10 +23,12 @@ const ProductCart = ({ product, showButton }) => {
           />
         </div>
 
-        <div className="p-4 flex flex-col md:flex-row justify-between items-center">
+        <div className="p-4 flex flex-col md:flex-row justify-between items-center text-left">
           <div>
-            <h4 className="text-[20px] italic text-purple-500 font-bold">{product.pname}</h4>
-            <h5 className="my-4 text-3xl font-bold italic text-trans">
+            <h4 className="text-[20px] italic text-purple-500 font-bold">
+              {product.pname}
+            </h4>
+            <h5 className="my-4 text-xl md:text-3xl font-bold italic text-trans">
               Price: ${product.price}
             </h5>
             <p className="text-lg leading-6 text-trans font-semibold italic">
